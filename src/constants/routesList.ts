3652e4ace5dc0f22d9940d41c9106eaf5461db23
Ruslan Goroshko <rusgoroshko@gmail.com>
2020-08-27 13:05:28 +0300

    User Profile & Log out

--- src/constants/routesList.ts
+++ src/constants/routesList.ts
@@ -7,6 +7,8 @@ import Markets from '../pages/Markets';
 import AccountsPage from "../pages/AccountsPage";
 import Portfolio from "../pages/Portfolio";
 import PositionDetails from "../pages/PositionDetails";
+import AccountProfile from '../pages/AccountProfile';
+import News from '../pages/News';
 
 export enum RouteLayoutType {
   Authorized,
@@ -24,13 +26,6 @@ const routesList = [
     strict: true,
     layoutType: RouteLayoutType.SignFlow,
   },
-  // {
-  //   component: LpLogin,
-  //   path: Page.LP_LOGIN,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.Public,
-  // },
   {
     component: SignUp,
     path: Page.SIGN_UP,
@@ -38,36 +33,22 @@ const routesList = [
     strict: true,
     layoutType: RouteLayoutType.Public,
   },
-  // {
-  //   component: EmailConfirmation,
-  //   path: Page.EMAIL_CONFIRMATION,
-  //   exact: false,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.Public,
-  // },
-   {
-     component: ForgotPassword,
-     path: Page.FORGOT_PASSWORD,
-     exact: true,
-     strict: true,
-     layoutType: RouteLayoutType.Public,
-   },
-  // {
-  //   component: ResetPassword,
-  //   path: Page.RESET_PASSWORD,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.Public,
-  // },
-   {
-     component: Dashboard,
-     path: Page.DASHBOARD,
-     exact: true,
-     strict: true,
-     layoutType: RouteLayoutType.Authorized,
-   },
+  {
+    component: ForgotPassword,
+    path: Page.FORGOT_PASSWORD,
+    exact: true,
+    strict: true,
+    layoutType: RouteLayoutType.Public,
+  },
+  {
+    component: Dashboard,
+    path: Page.DASHBOARD,
+    exact: true,
+    strict: true,
+    layoutType: RouteLayoutType.Authorized,
+  },
 
-   {
+  {
     component: AccountsPage,
     path: Page.ACCOUNTS_SWITCH,
     exact: true,
@@ -75,7 +56,7 @@ const routesList = [
     layoutType: RouteLayoutType.Authorized,
   },
 
-   {
+  {
     component: Markets,
     path: Page.MARKETS,
     exact: true,
@@ -105,46 +86,21 @@ const routesList = [
     strict: true,
     layoutType: RouteLayoutType.Authorized,
   },
-  // {
-  //   component: AccountBalance,
-  //   path: Page.ACCOUNT_BALANCE_HISTORY,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.Authorized,
-  // },
-  // {
-  //   component: AccountSecurity,
-  //   path: Page.ACCOUNT_SEQURITY,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.Authorized
-  // },
-  // {
-  //   component: Withdraw,
-  //   path: Page.ACCOUNT_WITHDRAW,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.Authorized
-  // },
-  // {
-  //   component: PersonalData,
-  //   path: Page.PERSONAL_DATA,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.KYC,
-  // },  {
-  //   component: PhoneVerification,
-  //   path: Page.PHONE_VERIFICATION,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.KYC,
-  // },  {
-  //   component: ProofOfIdentity,
-  //   path: Page.PROOF_OF_IDENTITY,
-  //   exact: true,
-  //   strict: true,
-  //   layoutType: RouteLayoutType.KYC,
-  // },
+
+  {
+    component: AccountProfile,
+    path: Page.ACCOUNT_PROFILE,
+    exact: false,
+    strict: true,
+    layoutType: RouteLayoutType.Authorized,
+  },
+  {
+    component: News,
+    path: Page.NEWS,
+    exact: false,
+    strict: true,
+    layoutType: RouteLayoutType.Authorized,
+  },
 ];
 
 export default routesList;
