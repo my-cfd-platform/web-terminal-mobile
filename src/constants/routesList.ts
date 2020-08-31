c03afc1668fd83e2b0096687252298f24660e887
Shadymination <shadi.ganem@simplbit.com>
2020-08-31 14:37:44 +0300

    withdraw and new placeholders

--- src/constants/routesList.ts
+++ src/constants/routesList.ts
@@ -4,12 +4,14 @@ import SignUp from '../pages/SignUp';
 import SingIn from '../pages/SignIn';
 import ForgotPassword from '../pages/ForgotPassword';
 import Markets from '../pages/Markets';
-import AccountsPage from "../pages/AccountsPage";
-import Portfolio from "../pages/Portfolio";
-import PositionDetails from "../pages/PositionDetails";
+import AccountsPage from '../pages/AccountsPage';
+import Portfolio from '../pages/Portfolio';
+import PositionDetails from '../pages/PositionDetails';
 import AccountProfile from '../pages/AccountProfile';
 import News from '../pages/News';
 import ChartSetting from '../pages/ChartSetting';
+import AccountBalanceHistory from '../pages/AccountBalanceHistory';
+import AccountWithdraw from '../pages/AccountWithdraw';
 
 export enum RouteLayoutType {
   Authorized,
@@ -107,6 +109,20 @@ const routesList = [
     component: News,
     path: Page.NEWS,
     exact: false,
+    strict: true,
+    layoutType: RouteLayoutType.Authorized,
+  },
+  {
+    component: AccountBalanceHistory,
+    path: Page.ACCOUNT_BALANCE_HISTORY,
+    exact: true,
+    strict: true,
+    layoutType: RouteLayoutType.Authorized,
+  },
+  {
+    component: AccountWithdraw,
+    path: Page.ACCOUNT_WITHDRAW,
+    exact: true,
     strict: true,
     layoutType: RouteLayoutType.Authorized,
   },
