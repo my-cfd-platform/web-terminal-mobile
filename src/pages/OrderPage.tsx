5790a58eb1ebc3315bfbd92fb7d4a26ae0127d26
Ruslan Goroshko <rusgoroshko@gmail.com>
2020-09-03 18:13:32 +0300

    invest input

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -356,7 +356,8 @@ const OrderPage = () => {
               {...getFieldProps(Fields.AMOUNT)}
               onBeforeInput={investOnBeforeInputHandler}
               onChange={investOnChangeHandler}
-              pattern="[.0-9]*"
+              // @ts-ignore
+              inputmode="decimal"
             />
           </InputWrap>
 
