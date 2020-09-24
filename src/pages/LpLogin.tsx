6b0a46321c0efa08af015eb59e4b4dac8a0dcd8c
Shadymination <shadi.ganem@simplbit.com>
2020-09-24 16:58:05 +0300

    https://monfex.atlassian.net/browse/WEBT-548

--- src/pages/LpLogin.tsx
+++ src/pages/LpLogin.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useStores } from '../hooks/useStores';
 import { OperationApiResponseCodes } from '../enums/OperationApiResponseCodes';
@@ -6,6 +6,7 @@ import Page from '../constants/Pages';
 import { useTranslation } from 'react-i18next';
 import API from '../helpers/API';
 import { getProcessId } from '../helpers/getProcessId';
+import { observer } from 'mobx-react-lite';
 
 interface QueryParams {
   lang: string;
@@ -13,29 +14,13 @@ interface QueryParams {
   page?: 'withdrawal' | 'deposit';
 }
 
-const LpLogin = () => {
+const LpLogin = observer(() => {
   const { token, lang, page } = useParams<QueryParams>();
   const { push } = useHistory();
-  const { mainAppStore } = useStores();
+  const { mainAppStore, userProfileStore } = useStores();
   const { i18n } = useTranslation();
 
   useEffect(() => {
-    async function getPersonalData() {
-      try {
-        const response = await API.getPersonalData(getProcessId());
-        const urlParams = new URLSearchParams();
-        urlParams.set('token', mainAppStore.token);
-        urlParams.set(
-          'active_account_id',
-          mainAppStore.accounts.find((item) => item.isLive)?.id || ''
-        );
-        urlParams.set('lang', mainAppStore.lang);
-        urlParams.set('env', 'web_mob');
-        urlParams.set('trader_id', response.data.id || '');
-        window.location.href = `${API_DEPOSIT_STRING}/?${urlParams.toString()}`;
-      } catch (error) {}
-    }
-
     async function fetchLpLogin() {
       try {
         const response = await mainAppStore.signInLpLogin({
@@ -44,7 +29,6 @@ const LpLogin = () => {
         if (response === OperationApiResponseCodes.Ok) {
           switch (page) {
             case 'deposit':
-              getPersonalData();
               break;
 
             case 'withdrawal':
@@ -68,7 +52,32 @@ const LpLogin = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (
+      page === 'deposit' &&
+      mainAppStore.isAuthorized &&
+      mainAppStore.accounts.length &&
+      userProfileStore.userProfileId
+    ) {
+      const urlParams = new URLSearchParams();
+      urlParams.set('token', mainAppStore.token);
+      urlParams.set(
+        'active_account_id',
+        mainAppStore.accounts.find((item) => item.isLive)?.id || ''
+      );
+      urlParams.set('lang', mainAppStore.lang);
+      urlParams.set('env', 'web_mob');
+      urlParams.set('trader_id', userProfileStore.userProfileId);
+      window.location.href = `${API_DEPOSIT_STRING}/?${urlParams.toString()}`;
+    }
+  }, [
+    page,
+    mainAppStore.accounts,
+    userProfileStore.userProfileId,
+    mainAppStore.isAuthorized,
+  ]);
+
   return <div></div>;
-};
+});
 
 export default LpLogin;
