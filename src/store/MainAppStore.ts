55d3e494421684c05bd2bedd09fe64ff91f3da53
Shadymination <shadi.ganem@simplbit.com>
2020-09-28 14:48:25 +0300

    slice -1 ?

--- src/store/MainAppStore.ts
+++ src/store/MainAppStore.ts
@@ -137,9 +137,7 @@ export class MainAppStore implements MainAppStoreProps {
 
   handleInitConnection = async (token = this.token) => {
     const wsConnectSub =
-      this.tradingUrl[this.tradingUrl.length - 1] === '/'
-        ? 'signalr'
-        : `/signalr`;
+      this.tradingUrl.slice(-1) === '/' ? 'signalr' : `/signalr`;
     const connectionString = IS_LIVE ? this.tradingUrl + wsConnectSub : WS_HOST;
     const connection = initConnection(connectionString);
 
