a4a07342344b9a67140ca948ee4708f741bcb13e
Ruslan Goroshko <rusgoroshko@gmail.com>
2020-10-27 11:26:48 +0200

    withdrawal kyc fix

--- src/pages/AccountWithdraw.tsx
+++ src/pages/AccountWithdraw.tsx
@@ -99,7 +99,7 @@ const AccountWithdraw = () => {
   return (
     // backLink={Page.ACCOUNT_PROFILE}
     <BackFlowLayout pageTitle={t('Withdraw')}>
-      {userProfileStore.userProfile?.kyc !== PersonalDataKYCEnum.Verified ? (
+      {userProfileStore.userProfile?.kyc === PersonalDataKYCEnum.NotVerified ? (
         <FlexContainer
           width="100%"
           height="100%"
