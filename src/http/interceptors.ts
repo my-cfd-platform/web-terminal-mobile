73e5129171ed769132160e10f8246a74e446db2c
Shadymination <shadi.ganem@simplbit.com>
2020-11-09 13:25:18 +0200

    safari ios cache bug fix

--- src/http/interceptors.ts
+++ src/http/interceptors.ts
@@ -104,6 +104,7 @@ const injectInerceptors = (tradingUrl: string, mainAppStore: MainAppStore) => {
     }
     config.headers[RequestHeaders.ACCEPT_LANGUAGE] = `${mainAppStore.lang}`;
     config.headers[RequestHeaders.CACHE_CONTROL] = 'no-cache';
+    config.headers[RequestHeaders.VARY] = '*';
 
     return config;
   });
