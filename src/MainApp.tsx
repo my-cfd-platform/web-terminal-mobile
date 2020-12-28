a7b0e0e023e26da613b86c72b272fd5c76bb774f
Shadymination <shadi.ganem@simplbit.com>
2020-12-28 14:57:01 +0200

    init

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -108,28 +108,32 @@ const MainApp: FC = () => {
   }, []);
 
   useEffect(() => {
-    new SmartBanner({
-      daysHidden: 15,   // days to hide banner after close button is clicked (defaults to 15)
-      daysReminder: 90, // days to hide banner after "VIEW" button is clicked (defaults to 90)
-      appStoreLanguage: 'us', // language code for the App Store (defaults to user's browser language)
-      title: 'MyPage',
-      author: 'MyCompany LLC',
-      button: 'VIEW',
-      store: {
-          ios: 'On the App Store',
-          android: 'In Google Play',
-          windows: 'In Windows store'
-      },
-      price: {
+    if (mainAppStore.initModel) {
+      let store = {};
+      if (mainAppStore.initModel.iosAppLink) {
+        store = { ...store, ios: mainAppStore.initModel.iosAppLink };
+      }
+      if (mainAppStore.initModel.androidAppLink) {
+        store = { ...store, android: mainAppStore.initModel.androidAppLink };
+      }
+      new SmartBanner({
+        daysHidden: 30, // days to hide banner after close button is clicked (defaults to 15)
+        daysReminder: 90, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+        appStoreLanguage: 'us', // language code for the App Store (defaults to user's browser language)
+        title: mainAppStore.initModel.brandName,
+        author: 'MyCompany LLC',
+        button: 'VIEW',
+        store: store,
+        price: {
           ios: 'FREE',
           android: 'FREE',
-          windows: 'FREE'
-      }
-      // , theme: '' // put platform type ('ios', 'android', etc.) here to force single theme on all device
-      // , icon: '' // full path to icon image if not using website icon image
-      // , force: 'ios' // Uncomment for platform emulation
-  });
-  }, [])
+        },
+        // , theme: '' // put platform type ('ios', 'android', etc.) here to force single theme on all device
+        // , icon: '' // full path to icon image if not using website icon image
+        // , force: 'ios' // Uncomment for platform emulation
+      });
+    }
+  }, [mainAppStore.initModel]);
 
   return (
     <>
