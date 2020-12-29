8ca7836c015d2984f6512f1841a529c3a22af0a4
Ruslan Goroshko <rusgoroshko@gmail.com>
2020-12-29 15:20:08 +0200

    test redirect with update refresh

--- src/hooks/useRedirectMiddleware.ts
+++ src/hooks/useRedirectMiddleware.ts
@@ -3,26 +3,27 @@ import { push } from "mixpanel-browser";
 import { useHistory } from "react-router-dom";
 
 const useRedirectMiddleware = () => {
-  const { push } = useHistory();
   const { mainAppStore } = useStores();
-  
+  const { push } = useHistory();
+  /*
+    link:   domain for redirect
+    params: url params string include 'token' that will be update
+  */
   const redirectWithUpdateRefreshToken = (link: string, params: string) => {
-
     console.log(mainAppStore.token)
-
     mainAppStore.postRefreshToken().finally(() => {
       console.log(mainAppStore.token);
-
       let searchParams = new URLSearchParams(params);
       searchParams.set("token", mainAppStore.token);
       console.log('redirect');
       const url = `${link}?${searchParams.toString()}`;
+      console.log(url)
       // push не открывает ссылку
-      window.location.href = url;
+      push(url)
     });
   }
 
-  return {redirectWithUpdateRefreshToken}
+  return { redirectWithUpdateRefreshToken };
 };
 
 export default useRedirectMiddleware;
