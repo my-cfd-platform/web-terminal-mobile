204243a45c5335a586bfc58862be6eec13225a07
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-02-24 15:10:25 +0200

    save

--- src/constants/listOfLanguages.ts
+++ src/constants/listOfLanguages.ts
@@ -11,6 +11,16 @@ export const ListForEN = {
     originName: 'Polski',
     shortName: 'PL',
   },
+  [CountriesEnum.ES]: {
+    name: 'Spanish',
+    originName: 'Español',
+    shortName: 'ES',
+  },
+  [CountriesEnum.DE]: {
+    name: 'German',
+    originName: 'Deutsch',
+    shortName: 'DE',
+  },
 };
 
 export const ListForPL = {
@@ -24,6 +34,16 @@ export const ListForPL = {
     originName: 'Polski',
     shortName: 'PL',
   },
+  [CountriesEnum.ES]: {
+    name: 'Hiszpański',
+    originName: 'Español',
+    shortName: 'ES',
+  },
+  [CountriesEnum.DE]: {
+    name: 'Niemiecki',
+    originName: 'Deutsch',
+    shortName: 'DE',
+  },
 };
 
 export const ListForES = {
@@ -36,6 +56,40 @@ export const ListForES = {
     name: 'Polaco',
     originName: 'Polski',
     shortName: 'PL',
+  },
+  [CountriesEnum.ES]: {
+    name: 'Español',
+    originName: 'Español',
+    shortName: 'ES',
+  },
+  [CountriesEnum.DE]: {
+    name: 'Alemán',
+    originName: 'Deutsch',
+    shortName: 'DE',
+  },
+};
+
+
+export const ListForDE = {
+  [CountriesEnum.EN]: {
+    name: 'Inglés',
+    originName: 'English',
+    shortName: 'EN',
+  },
+  [CountriesEnum.PL]: {
+    name: 'Polski',
+    originName: 'Polieren',
+    shortName: 'PL',
+  },
+  [CountriesEnum.ES]: {
+    name: 'Español',
+    originName: 'Español',
+    shortName: 'ES',
+  },
+  [CountriesEnum.DE]: {
+    name: 'Deutsch',
+    originName: 'Deutsch',
+    shortName: 'DE',
   },
 };
 
