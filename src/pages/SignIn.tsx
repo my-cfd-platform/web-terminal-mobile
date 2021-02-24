c4e4c18b5c0535a5bf8cb47847523a2ed1f4dfe5
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-02-24 15:26:39 +0200

    save

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -178,7 +178,7 @@ const SignIn = () => {
               fontWeight="bold"
               fontSize="16px"
             >
-              {t('Log In')}
+              {t('Log in')}
             </PrimaryTextSpan>
           </PrimaryButton>
         </FlexContainer>
@@ -192,7 +192,7 @@ const SignIn = () => {
             {t('Don`t have an account yet?')}
           </PrimaryTextSpan>
           &nbsp;
-          <StyledLink to={Page.SIGN_UP}>{t('Sign Up')}</StyledLink>
+          <StyledLink to={Page.SIGN_UP}>{t('Sign up')}</StyledLink>
         </FlexContainer>
       </FlexContainer>
     </FlexContainer>
