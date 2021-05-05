f51da3f1acfe8e939b0b53aac3e9b8aef59b417b
Anton_Tesenchuk <anton.tesenchuk@simplbit.com>
2021-05-05 18:42:16 +0300

    WEBT-1234 Make it hard

--- src/helpers/API.ts
+++ src/helpers/API.ts
@@ -491,11 +491,12 @@ class API {
     console.log(API_MISC_STRING);
     console.log(miscUrl);
     const response = await axios.get<OnBoardingInfo>(
-      `${
-        API_MISC_STRING.length ?
-          API_MISC_STRING :
-          miscUrl
-      }${API_LIST.ONBOARDING.STEPS}/${stepNumber}?deviceTypeId=${deviceType}`
+      // `${
+      //   API_MISC_STRING.length ?
+      //     API_MISC_STRING :
+      //     miscUrl
+      // }${API_LIST.ONBOARDING.STEPS}/${stepNumber}?deviceTypeId=${deviceType}`
+      `https://trading-api-misc-test.mnftx.biz${API_LIST.ONBOARDING.STEPS}/${stepNumber}?deviceTypeId=${deviceType}`
     );
     return response.data;
   };
