92843d60c25546c15c76a6b6129cc5d57fd9a8b3
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-08-25 21:58:39 +0300

    https://smplt.atlassian.net/browse/WEBT-1462

--- src/http/interceptors.ts
+++ src/http/interceptors.ts
@@ -10,31 +10,11 @@ import { getProcessId } from '../helpers/getProcessId';
 import API from '../helpers/API';
 import { getCircularReplacer } from '../helpers/getCircularReplacer';
 import { getStatesSnapshot } from '../helpers/getStatesSnapshot';
-import requestOptions from '../constants/requestOptions';
-import mixpanelEvents from '../constants/mixpanelEvents';
-import mixapanelProps from '../constants/mixpanelProps';
-import mixpanel from 'mixpanel-browser';
-
-const openNotification = (errorText: string, mainAppStore: MainAppStore) => {
-  mainAppStore.rootStore.notificationStore.setNotification(errorText);
-  mainAppStore.rootStore.notificationStore.isSuccessfull = false;
-  mainAppStore.rootStore.notificationStore.openNotification();
-};
 
 const injectInerceptors = (mainAppStore: MainAppStore) => {
   // for multiple requests
   let isRefreshing = false;
   let failedQueue: any[] = [];
-
-  const getApiUrl = (url: string) => {
-    const urlString = new URL(url);
-    if (urlString.search) {
-      return urlString.href
-        .split(urlString.search)[0]
-        .split(urlString.origin)[1];
-    }
-    return urlString.href.split(urlString.origin)[1];
-  };
 
   const processQueue = (error: any, token: string | null = null) => {
     failedQueue.forEach((prom) => {
@@ -44,51 +24,50 @@ const injectInerceptors = (mainAppStore: MainAppStore) => {
         prom.resolve(token);
       }
     });
+
     failedQueue = [];
   };
 
   // TODO: research init flow
   axios.interceptors.response.use(
-    function (response: AxiosResponse) {
-      switch (response.data.result) {
-        case OperationApiResponseCodes.TechnicalError:
-          return Promise.reject(
-            apiResponseCodeMessages[OperationApiResponseCodes.TechnicalError]
-          );
-        case OperationApiResponseCodes.InvalidUserNameOrPassword:
-          mainAppStore.signOut();
-          break;
-        default:
-          break;
+    function (config: AxiosResponse) {
+      if (config.data.result === OperationApiResponseCodes.TechnicalError) {
+        axios.request(config.config);
+        if (!mainAppStore.rootStore.serverErrorPopupStore.isActive) {
+          mainAppStore.rootStore.serverErrorPopupStore.openModal();
+        }
+        setTimeout(() => {
+          axios.request(config.config);
+          if (!mainAppStore.rootStore.serverErrorPopupStore.isActive) {
+            mainAppStore.rootStore.serverErrorPopupStore.openModal();
+          }
+        }, +mainAppStore.connectTimeOut);
+        return Promise.reject(
+          apiResponseCodeMessages[OperationApiResponseCodes.TechnicalError]
+        );
       }
-      return response;
+      if (
+        config.data.result ===
+        OperationApiResponseCodes.InvalidUserNameOrPassword
+      ) {
+        mainAppStore.signOut();
+      }
+
+      if (config.data.result === OperationApiResponseCodes.Ok) {
+        if (mainAppStore.rootStore.serverErrorPopupStore.isActive) {
+          mainAppStore.rootStore.serverErrorPopupStore.closeModal();
+        }
+      }
+      return config;
     },
 
     async function (error) {
-      const excludeCheckErrorFlow = [
-        API_LIST.DEBUG.POST,
-        API_LIST.ONBOARDING.STEPS,
-        API_LIST.WELCOME_BONUS.GET
-      ];
-
-      const requestUrl: string = error?.config?.url;
-      const originalRequest = error.config;
-
-      const repeatRequest = (callback: any) => {
-        setTimeout(() => {
-          callback(originalRequest);
-        }, +mainAppStore.connectTimeOut);
-      };
-
-     if (excludeCheckErrorFlow.includes(getApiUrl(requestUrl))) {
-        return Promise.reject(error);
+      if (error.response?.config?.url.includes(API_LIST.DEBUG.POST)) {
+        return await Promise.reject(error);
       }
-
-      // logger
       if (
         mainAppStore.isAuthorized &&
-        !doNotSendRequest.includes(error.response?.status) &&
-        (error.response?.status || error.config?.timeoutErrorMessage)
+        !doNotSendRequest.includes(error.response?.status)
       ) {
         const objectToSend = {
           message: error.message,
@@ -112,85 +91,38 @@ const injectInerceptors = (mainAppStore: MainAppStore) => {
         };
         API.postDebug(params);
       }
-      // --- logger
-
-      // check for formData
-      let finalJSON = '';
-      if (typeof error.config.data === 'object') {
-        const dataObject = {};
-        error.config.data.forEach((value: any, key: any) => {
-          // @ts-ignore
-          dataObject[key] = value
-        });
-        finalJSON = JSON.stringify(dataObject);
-      } else {
-        finalJSON = error.config.data;
+      if (!error.response?.status) {
+        mainAppStore.rootStore.badRequestPopupStore.setRecconect();
+        setTimeout(() => {
+          axios.request(error.config);
+          mainAppStore.rootStore.badRequestPopupStore.stopRecconect();
+        }, +mainAppStore.connectTimeOut);
       }
 
-      const isTimeOutError: boolean = error.message === requestOptions.TIMEOUT;
-      const isReconnectedRequest: boolean =
-        JSON.parse(finalJSON).initBy === requestOptions.BACKGROUND;
+      const originalRequest = error.config;
 
-      if (isTimeOutError && isReconnectedRequest) {
-        return new Promise((resolve) => {
-          repeatRequest(() => {
-            resolve(axios(originalRequest));
-          });
-        });
-      }
-
-      if (isTimeOutError && !isReconnectedRequest) {
-        openNotification('Timeout connection error', mainAppStore);
-      }
-
-      const urlString = new URL(error?.config?.url).href;
-      // mixpanel
-      if (isTimeOutError) {
-        mixpanel.track(mixpanelEvents.TIMEOUT, {
-          [mixapanelProps.REQUEST_URL]: urlString,
-        });
-      }
-
-      if (error.response?.status) {
-        if (error.response?.status.toString().includes('50')) {
-          mixpanel.track(mixpanelEvents.SERVER_ERROR_50X, {
-            [mixapanelProps.REQUEST_URL]: urlString,
-            [mixapanelProps.ERROR_TEXT]: error.response?.status,
-          });
-        }
-        if (error.response?.status.toString().includes('40')) {
-          mixpanel.track(mixpanelEvents.SERVER_ERROR_40X, {
-            [mixapanelProps.REQUEST_URL]: urlString,
-            [mixapanelProps.ERROR_TEXT]: error.response?.status,
-          });
-        }
-      }
-      // --- mixpanel
-
-      if (!error.response?.status && !isTimeOutError && !isReconnectedRequest) {
-        openNotification(error.message, mainAppStore);
-      }
-
-      if (error.response?.status) {
-        if (
-          (error.response?.status !== 401 &&
-            error.response?.status !== 403 &&
-            error.response?.status.toString().includes('40')) ||
-          error.response?.status.toString().includes('50')
-        ) {
-          if (isReconnectedRequest) {
-            return new Promise((resolve) => {
-              repeatRequest(() => {
-                resolve(axios(originalRequest));
-              });
-            });
-          } else {
-            mainAppStore.rootStore.serverErrorPopupStore.openModal();
-          }
-        }
+      if (error.response?.config?.url.includes(API_LIST.ONBOARDING.STEPS)) {
+        return Promise.reject(error);
       }
 
       switch (error.response?.status) {
+        case 400:
+        case 500:
+          if (
+            !error.response?.config?.url.includes(API_LIST.WELCOME_BONUS.GET)
+          ) {
+            function requestAgain() {
+              axios.request(error.config);
+              if (!mainAppStore.rootStore.serverErrorPopupStore.isActive) {
+                mainAppStore.rootStore.serverErrorPopupStore.openModal();
+              }
+            }
+            setTimeout(requestAgain, +mainAppStore.connectTimeOut);
+            mainAppStore.isLoading = false;
+          }
+
+          break;
+
         case 401:
           if (mainAppStore.refreshToken && !originalRequest._retry) {
             if (isRefreshing) {
