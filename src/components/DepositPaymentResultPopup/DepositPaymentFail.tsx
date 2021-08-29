daf6747dde5221742a3b829c31adbb1d7d996c53
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-08-29 15:01:22 +0300

    add icons deposit result

--- src/components/DepositPaymentResultPopup/DepositPaymentFail.tsx
+++ src/components/DepositPaymentResultPopup/DepositPaymentFail.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
-import FailImage from '../../assets/images/fail.png';
 import { FlexContainer } from '../../styles/FlexContainer';
 import { useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -9,11 +8,28 @@ import { useStores } from '../../hooks/useStores';
 import Page from '../../constants/Pages';
 import mixpanel from 'mixpanel-browser';
 import mixpanelEvents from '../../constants/mixpanelEvents';
+import * as FailImage from '../../assets/lotties/fail-icon.json';
+import Lottie from 'react-lottie';
 
 const DepositPaymentFail: FC = () => {
   const { mainAppStore, userProfileStore } = useStores();
   const [parsedParams, setParsedParams] = useState('');
   const { replace } = useHistory();
+
+  const getLottieIconOptions = () => {
+    return {
+      loop: false,
+
+      autoplay: true,
+      pause: false,
+      animationData: FailImage.default,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+        clearCanvas: false,
+      },
+    };
+  };
+
   useEffect(() => {
     urlParams.set('token', mainAppStore.token);
     urlParams.set(
@@ -57,7 +73,12 @@ const DepositPaymentFail: FC = () => {
           marginBottom="40px"
           height="138px"
         >
-          <img src={FailImage} width={138} />
+          <Lottie
+            options={getLottieIconOptions()}
+            height="136px"
+            width="136px"
+            isClickToPauseDisabled={true}
+          />
         </FlexContainer>
         <FailText>{t('Failed')}</FailText>
         <FailDescription>
