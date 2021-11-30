dbdcadcf1f7fd229e48da06d0bc43f26d5c2cad8
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-12-01 00:16:38 +0200

    https://smplt.atlassian.net/browse/WEBT-1669

--- src/pages/AccountProfile.tsx
+++ src/pages/AccountProfile.tsx
@@ -30,6 +30,7 @@ import useRedirectMiddleware from '../hooks/useRedirectMiddleware';
 import { CountriesEnum } from '../enums/CountriesEnum';
 import AccProfileCarousel from '../components/AccProfileCarousel/AccProfileCarousel';
 import ConfirmationBottomModal from '../components/Modals/ConfirmationBottomModal';
+import AccountKYCProfileLabel from '../components/AccountVerification/AccountKYCProfileLabel';
 
 const AccountProfile = observer(() => {
   const { mainAppStore, userProfileStore } = useStores();
@@ -148,38 +149,7 @@ const AccountProfile = observer(() => {
         <AccProfileCarousel />
       )}
 
-      {!mainAppStore.isPromoAccount &&
-        userProfileStore.userProfile?.kyc ===
-          PersonalDataKYCEnum.NotVerified && (
-          <FlexContainer flexDirection="column" marginBottom="24px">
-            <ProfileMenuLink to={Page.ACCOUNT_VERIFICATION}>
-              <FlexContainer alignItems="center">
-                <FlexContainer
-                  width="28px"
-                  height="28px"
-                  backgroundColor="#00000000"
-                  borderRadius="50%"
-                  justifyContent="center"
-                  alignItems="center"
-                  marginRight="14px"
-                >
-                  <SvgIcon {...IconVerify} fillColor="#ED145B" />
-                </FlexContainer>
-                <PrimaryTextSpan
-                  color="#ffffff"
-                  fontSize="16px"
-                  fontWeight="normal"
-                >
-                  {t('Fill in personal details')}
-                </PrimaryTextSpan>
-              </FlexContainer>
-              <SvgIcon
-                {...IconArrowLink}
-                fillColor="rgba(196, 196, 196, 0.5)"
-              />
-            </ProfileMenuLink>
-          </FlexContainer>
-        )}
+      {!mainAppStore.isPromoAccount && <AccountKYCProfileLabel />}
 
       {!mainAppStore.isPromoAccount && (
         <FlexContainer flexDirection="column" marginBottom="24px">
