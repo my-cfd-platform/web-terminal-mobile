01226ed08e3c4dd41f5e0f55a821bafef80d5f6b
Ruslan Goroshko <rusgoroshko@gmail.com>
2021-12-17 17:53:31 +0200

    add status table tooltips

--- src/constants/AccountStatusTableTooltip.ts
+++ src/constants/AccountStatusTableTooltip.ts
@@ -5,47 +5,52 @@ interface AccountStatusTableTooltipProps {
   blockWidth: string;
 }
 
-export const AccountStatusTableTooltip: AccountStatusTableTooltipProps[]  = [
+export const AccountStatusTableTooltip: AccountStatusTableTooltipProps[] = [
   {
-    id: "deposit",
-    label: "Deposit",
-    tooltipText: "test deposit",
-    blockWidth: "144px"
+    id: 'deposit',
+    label: 'Deposit',
+    tooltipText:
+      'This is the amount of funds that will be available for you to trade with',
+    blockWidth: '144px',
   },
   {
-    id: "instruments",
-    label: "Instruments",
-    tooltipText: "test",
-    blockWidth: "144px"
+    id: 'instruments',
+    label: 'Instruments',
+    tooltipText: 'Number of Assets that can be traded',
+    blockWidth: '144px',
   },
   {
-    id: "personal_session",
-    label: "Personal session",
-    tooltipText: "test",
-    blockWidth: "172px"
+    id: 'personal_session',
+    label: 'Personal session',
+    tooltipText:
+      'Type of included personal educational session with an investment manager',
+    blockWidth: '172px',
   },
   {
-    id: "webinars",
-    label: "Webinars",
-    tooltipText: "test",
-    blockWidth: "144px"
+    id: 'webinars',
+    label: 'Webinars',
+    tooltipText:
+      'Live sessions with a professional trader during market events or promotions',
+    blockWidth: '144px',
   },
   {
-    id: "analytics",
-    label: "Analytics",
-    tooltipText: "test",
-    blockWidth: "144px"
+    id: 'analytics',
+    label: 'Analytics',
+    tooltipText:
+      'Access to assisted trading software, indicators, analytics  and special charting tools',
+    blockWidth: '144px',
   },
   {
-    id: "spread",
-    label: "Spread",
-    tooltipText: "test",
-    blockWidth: "100px"
+    id: 'spread',
+    label: 'Spread',
+    tooltipText:
+      'A discount on the commission that occurs from the difference between Bid and Ask prices',
+    blockWidth: '100px',
   },
   {
-    id: "swap",
-    label: "Swap",
-    tooltipText: "test",
-    blockWidth: "100px"
+    id: 'swap',
+    label: 'Swap',
+    tooltipText: 'A discount on the over-the-night transactions',
+    blockWidth: '100px',
   },
 ];
